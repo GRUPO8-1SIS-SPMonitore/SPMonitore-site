8beb68ed4723a8f6ee787f65d9d4c10a2a4e7142
deboraflores <debora.flores@sptech.school>
2022-05-06 10:44:28 -0300

    site oficial, final

--- SITE COMPLETO - SPMonitore/function.js
+++ SITE COMPLETO - SPMonitore/function.js
@@ -1,30 +1,56 @@
-var user;
-var pass;
+function cadastro(){
 
-function Cadastro(){
-    var nome = nome-cad.value
-    var numero = num-cad.value
-    if(indexOf('@' && '.com' )){
-        var email = email-cad.value
+    if(nome_cad.value.length >= 3){
+        var nome = nome_cad.value
+    }else{
+        alert('Nome muito curto!')
+        return limparCampo()
+    }
+        if(num_cad.value.length == 11 ||  num_cad.value.length == 13){
+        var numero = num_cad.value
+    }else{
+        alert('Número inválido')
+        return limparCampo()
+    }
+    
+    if(email_cad.value.indexOf('@') > 0 && email_cad.value.indexOf('.com') > 0){
+        var email = email_cad.value
     }else{
         alert("Email inválido")
+        return limparCampo()
     }
     
-    if(senha-cad.value == conf-senha.value){
-        var senha = conf-senha.value
+    if(senha_cad.value == conf_senha.value){
+        var senha = conf_senha.value
     }else{
         alert("As senhas estão diferentes!")
-    }if(nome-cad.value == "" || email-cad.value == "" || num-cad == "" || senha-cad == "" || conf-senha == ""){
+        return limparCampo()
+    }if(nome_cad.value == "" || email_cad.value == "" || num_cad == "" || senha_cad == "" || conf_senha == ""){
         alert("É necessário preencher todos os campos!")
+        return limparCampo()
     }else{
         alert("Cadastro realizado, faça seu login")
-        window.location.href="login.html"
-        user = email
-        pass = senha
+        location="login.html"
     }
 }
 
-function Login(){
-    
+function login(){
+    var user = user_log.value
+    var pass = pass_log.value
+    if(user == 'teste@gmail.com' && pass == '1234'){
+       location ="dashboard_tempo_real.html"
+    }else{
+        alert('Usuário ou senha incorretos')
+        return
+    }
 }
 
+function limparCampo(){
+    nome_cad.value = "" 
+    email_cad.value = "" 
+    num_cad.value = "" 
+    senha_cad.value = "" 
+    conf_senha.value = ""
+}
+
+                                   
